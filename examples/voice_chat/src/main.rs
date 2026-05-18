fdf7b5ff200ba572c692ba505b88ef3eade456e8
IRSMsoso <cameronvandertuig@proton.me>
2026-05-18 15:51:33 -0700

    Fill out voice chat example

--- examples/voice_chat/src/main.rs
+++ examples/voice_chat/src/main.rs
@@ -1,5 +1,5 @@
+use clap::Parser;
 use firewheel::cpal::CpalStream;
-use firewheel::nodes::sampler::{SamplerNode, SamplerState};
 use firewheel::FirewheelContext;
 use firewheel_network_node::nodes::receiver_node::{
     NetworkReceiverNode, NetworkReceiverNodeConfig,
@@ -11,39 +11,40 @@ use firewheel_network_node::nodes::transmitter_node::{
 use firewheel_network_node::transport::udp_socket_transport::{
     UdpSocketTransport, UdpSocketTransportConfig,
 };
-use log::error;
+use log::{error, info};
 use std::net::Ipv4Addr;
 use std::time::Duration;
 
 const UPDATE_INTERVAL: Duration = Duration::from_millis(15);
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    address: Ipv4Addr,
+}
+
 fn main() {
     simple_logger::SimpleLogger::new().env().init().unwrap();
+
+    let cli = Cli::parse();
 
     // --- Start the context and get the sample rate of the audio stream. ----------------
 
     let mut cx = FirewheelContext::new(Default::default());
     let mut stream = CpalStream::new(&mut cx, Default::default()).unwrap();
 
-    println!("Sample rate: {}", cx.stream_info().unwrap().sample_rate);
-
-    let sample_rate = cx.stream_info().unwrap().sample_rate;
-
-    let mut sampler_node = SamplerNode::default();
-
-    let sampler_id = cx
-        .add_node(sampler_node, None)
-        .expect("Sampler node should construct without error");
+    info!("Sample rate: {}", cx.stream_info().unwrap().sample_rate);
 
     let transmitter_node: NetworkTransmitterNode<UdpSocketTransport> =
-        NetworkTransmitterNode::new(Ipv4Addr::new(127, 0, 0, 1).into(), 0);
+        NetworkTransmitterNode::new(cli.address.into(), 0);
 
     let transmitter_id = cx
         .add_node(
             transmitter_node,
             Some(NetworkTransmitterNodeConfig {
                 channels: OpusChannels::Mono,
-                opus_application_type: OpusApplicationType::Audio,
+                opus_application_type: OpusApplicationType::Voip,
                 transport_config: UdpSocketTransportConfig { port: 1680 },
             }),
         )
@@ -61,10 +62,11 @@ fn main() {
         )
         .unwrap();
 
+    let graph_in_id = cx.graph_in_node_id();
     let graph_out_id = cx.graph_out_node_id();
 
-    // Connect sampler to transmitter
-    cx.connect(sampler_id, transmitter_id, &[(0, 0), (1, 0)], false)
+    // Connect input to transmitter
+    cx.connect(graph_in_id, transmitter_id, &[(0, 0), (1, 0)], false)
         .unwrap();
 
     // Connect receiver to output
